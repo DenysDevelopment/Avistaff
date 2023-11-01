5f4831b241e80f9e6c3c2ebb1f46ffe2e35798f6
Денис Максимук <59338803+DenysDevelopment@users.noreply.github.com>
2023-11-01 18:37:43 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,157 @@
+const isMobile = {
+	Android: function () {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Android() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows()
+		);
+	},
+};
+
+//========================================================================================================================================================
+
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.map((entry) => {
+			if (entry.isIntersecting) {
+				const id = entry.target.id;
+
+				const target = document.querySelector(`[href='#${id}']`);
+				changeMenuSlider(target.offsetLeft, target.offsetWidth);
+			}
+		});
+	},
+	{
+		threshold: 0.7,
+	},
+);
+
+document.querySelectorAll("section").forEach((section) => {
+	observer.observe(section);
+});
+
+//========================================================================================================================================================
+
+const gotoSection = () => {
+	const links = document.querySelectorAll("[data-goto-section]");
+
+	links.forEach((link) => {
+		link.addEventListener("click", (e) => {
+			const header = document.querySelector(".fixed");
+			if (link.dataset.gotoSection) {
+				e.preventDefault();
+				const section = document.getElementById(e.target.dataset.gotoSection);
+				const sectionTop = header
+					? section.getBoundingClientRect().top - header.offsetHeight
+					: section.getBoundingClientRect().top;
+
+				document.body.classList.remove("page__body--lock");
+				const iconMenu = document
+					.querySelector(".menu__icon")
+					.classList.remove("menu__icon--active");
+				const menuBody = document
+					.querySelector(".menu__body")
+					.classList.remove("menu__body--active");
+
+				window.scrollBy({
+					top: sectionTop,
+					behavior: "smooth",
+				});
+			}
+		});
+	});
+};
+gotoSection();
+
+//========================================================================================================================================================
+
+if (isMobile.any()) {
+	document.body.classList.add("page__body--touch");
+
+	const menuArrow = document.querySelectorAll(".menu__arrow");
+
+	menuArrow.forEach((arrow) => {
+		arrow.addEventListener("click", () => {
+			arrow.parentElement.classList.toggle("menu__item--active");
+		});
+	});
+} else {
+	document.body.classList.add("page__body--pc");
+}
+
+const iconMenu = document.querySelector(".menu__icon");
+if (iconMenu != null) {
+	const menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", () => {
+		document.body.classList.toggle("page__body--lock");
+		iconMenu.classList.toggle("menu__icon--active");
+		menuBody.classList.toggle("menu__body--active");
+	});
+}
+
+let body = document.body,
+	html = document.documentElement;
+
+let height = Math.max(
+	body.scrollHeight,
+	body.offsetHeight,
+	html.clientHeight,
+	html.scrollHeight,
+	html.offsetHeight,
+);
+
+const header = document.querySelector(".header");
+if (header) {
+	if (height > 1500) {
+		window.addEventListener("scroll", (e) => {
+			if (window.pageYOffset > 200) {
+				header.classList.add("fixed");
+				document.body.style.paddingTop = `${header.offsetHeight}px`;
+			} else {
+				header.classList.remove("fixed");
+				document.body.style.paddingTop = `0px`;
+			}
+		});
+	}
+}
+
+//========================================================================================================================================================
+
+const menuSlider = document.querySelector(".menu__slider");
+const links = document.querySelectorAll("[data-goto-section]");
+
+const changeMenuSlider = (left = 0, width = 100) => {
+	menuSlider.style.left = `${left}px`;
+	menuSlider.style.width = `${width}px`;
+};
+
+// links.forEach((link) => {
+// 	link.addEventListener("click", (e) => {
+// 		const target = e.target;
+// 		changeMenuSlider(target.offsetLeft, target.clientWidth);
+// 	});
+// });
+
+changeMenuSlider(links[0].offsetLeft, links[0].clientWidth);
+
+//========================================================================================================================================================
+
+new WOW().init();
+//========================================================================================================================================================
---
+++ telegram.php
@@ -0,0 +1,39 @@
+<?php
+
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['email'];
+
+// $url = "https://www.google.com/recaptcha/api/siteverify";
+// $key = "6Leq6ngiAAAAAJiZuTCNr7dUcZpX0iiP2yGhagEh";
+// $query = $url.'?secret='.$key.'&response='.$_POST['g-recaptcha-response']."&remoteip=".$_SERVER['REMOTE_ADDR'];
+
+// if(!$_POST['g-recaptcha-response']) {
+//   header("Location: /#form");
+//   $_SESSION['recaptcha'] = 'captcha nie przeszedł';
+//   exit();
+// }
+
+// $data = json_decode(file_get_contents($query));
+// if($data->success == false) {
+//   header("Location: /#form");
+//   $_SESSION['recaptcha'] = 'captcha nie przeszedł';
+//   exit();
+// }
+
+// $_SESSION['recaptcha'] = "";
+$token = "6792498583:AAEzowTFZS9a5C2tXxrMndM4JfvH6Tq8KFY";
+$chat_id = "-4026639687";
+$arr = array(
+  '<b>Imię:</b> ' => $name,
+  '<b>Tel:</b> ' => $tel,
+  '<b>Email:</b> ' => $email,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+?>
